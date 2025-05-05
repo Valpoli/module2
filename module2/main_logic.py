4d2251987048578cd2a7cfe19a82901bb9a9c45a
Valpoli <valentin@pc-valentin.>
2025-05-05 10:49:44 +0200

    y

--- module2/main_logic.py
+++ module2/main_logic.py
@@ -1,5 +1,7 @@
 import sys
 import os
+
+# Ajoute le dossier parent à sys.path pour trouver moduleA et moduleB
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from moduleA import do_a_stuff
@@ -9,3 +11,6 @@ def fusion_module_divin():
     print("=== Fusion divine des modules ===")
     print("Module A dit :", do_a_stuff())
     print("Module B répond :", do_b_stuff())
+
+if __name__ == "__main__":
+    fusion_module_divin()
